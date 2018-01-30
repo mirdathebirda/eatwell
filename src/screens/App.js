9695e60e5d0cab02dee180cae33c37f2fa7e4f72
mirdathebirda <mirandachiu13@gmail.com>
2018-01-29 21:48:02 -0500

    got 3 buttons that cant do anything

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Link } from 'react-router';
+import { Constants } from 'expo';
+import { WebBrowser } from 'expo';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Meal Planning </Text>
-        <Text>Nutrition and Cheatsheets</Text>
-        <Text>How Tos </Text>
+      <View style=styles.button>
+        <Button
+        large
+        icon={{name: 'envira', type: 'font-awesome'}}
+        title='Meal Planning' />
+
+        <Button
+        large
+        icon={{name: 'envira', type: 'font-awesome'}}
+        title='Nutrition and Cheatsheets' />
+
+        <Button
+        large
+        icon={{name: 'envira', type: 'font-awesome'}}
+        title='How Tos' />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  button: {
+    // marginTop:Constants.statusBarHeight,
+  	marginBottom: Constants.statusBarHeight,
   },
 });
