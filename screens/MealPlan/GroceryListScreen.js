ab63d6aa1e093f0de2a832c04e55e4a120e9b6cf
mirdathebirda <mirandachiu13@gmail.com>
2018-03-11 18:26:03 -0400

    checkboxes but not..interactive..???

--- screens/MealPlan/GroceryListScreen.js
+++ screens/MealPlan/GroceryListScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { ProximaText } from '../../components/StyledText';
+import CheckBox from 'react-native-checkbox';
 
 
 export default class GroceryListScreen extends React.Component {
@@ -11,27 +12,28 @@ export default class GroceryListScreen extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
 				<ProximaText style={styles.header}> Produce </ProximaText>
-				<ProximaText> 4oz broccoli </ProximaText>
-				<ProximaText> 2 beefsteak tomatoes </ProximaText>
+				<CheckBox label=' 4oz broccoli' checked={false} />
+				<CheckBox label=' 2 beefsteak tomatoes' checked={false} />
 
 				<ProximaText style={styles.header}> Meat </ProximaText>
-				<ProximaText> 1lb of chicken </ProximaText>
+				<CheckBox label=' 1lb of chicken' checked={false} />
 
 				<ProximaText style={styles.header}> Dairy </ProximaText>
-				<ProximaText>1 cup buttermilk </ProximaText>
-				<ProximaText> 1 large egg</ProximaText>
+				<CheckBox label='1 cup buttermilk' checked={false} />
+				<CheckBox label=' 1 large egg' checked={false} />
 
 				<ProximaText style={styles.header}>Dry/Baking Goods</ProximaText>
-				<ProximaText> 1 cup Panko breadcrumbs </ProximaText>
-				<ProximaText> 3/4 all-purpose flour </ProximaText>
-				<ProximaText> 1/2 up cornstarch </ProximaText>
+				<CheckBox label=' 1 cup Panko breadcrumbs' checked={false} />
+				<CheckBox label='3/4 all-purpose flour' checked={false} />
+				<CheckBox label='1/2 cup cornstarch' checked={false} />
 
 				<ProximaText style={styles.header}>Spices and Sauces</ProximaText>
-				<ProximaText>1/2 cup mayonaise</ProximaText>
-				<ProximaText>1/4 cup sweet chili sauce</ProximaText>
-				<ProximaText>1 tbsp hot sauce (Sriracha)</ProximaText>
+				<CheckBox label='1/2 cup mayonaise' checked={false} />
+				<CheckBox label='1/4 cup sweet chili sauce' checked={false} />
+				<CheckBox label='1 tbsp hot sauce (Sriracha)' checked={false} />
+
 			</View>
     );
   }
@@ -40,6 +42,8 @@ export default class GroceryListScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+		paddingLeft:10,
+		paddingRight:10,
     paddingTop: 15,
     backgroundColor: '#fff',
   },
