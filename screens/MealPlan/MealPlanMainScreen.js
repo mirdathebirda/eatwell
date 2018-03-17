f78eb93be3624f8c9662c4278302a4b35a03baeb
mirdathebirda <mirandachiu13@gmail.com>
2018-03-17 13:03:04 -0400

    meal plan main

--- screens/MealPlan/MealPlanMainScreen.js
+++ screens/MealPlan/MealPlanMainScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { Image, TouchableOpacity, ScrollView, StyleSheet, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import Colors from '../../constants/Colors';
 import { ProximaText } from '../../components/StyledText';
+
+import PercentageCircle from 'react-native-percentage-circle';
 
 
 export default class MealPlanMainScreen extends React.Component {
@@ -12,9 +15,87 @@ export default class MealPlanMainScreen extends React.Component {
 	render() {
 		return (
 			<ScrollView style={styles.container}>
-				{/* Go ahead and delete ExpoLinksView and replace it with your
-					 * content, we just wanted to provide you with some helpful links */}
-				<ExpoLinksView />
+				<View style={styles.datebar}>
+					<ProximaText style={styles.date}> 12/3 - 12/9 </ProximaText>
+				</View>
+				<View style = {styles.mealplanBar}>
+					<ProximaText style = {styles.header}> This Week's Meal Plan </ProximaText>
+					<View style={styles.pcircles} >
+						<View style={styles.pcircle} >
+							<PercentageCircle borderWidth={5} radius={25} percent={75} color={Colors.navy}></PercentageCircle>
+							<ProximaText> Carbs </ProximaText>
+						</View>
+						<View style={styles.pcircle} >
+							<PercentageCircle borderWidth={5} radius={25} percent={54} color={Colors.navy}></PercentageCircle>
+							<ProximaText> Protein </ProximaText>
+						</View>
+						<View style={styles.pcircle} >
+							<PercentageCircle borderWidth={5} radius={25} percent={92} color={Colors.navy}></PercentageCircle>
+							<ProximaText> Fat </ProximaText>
+						</View>
+						<View style={styles.pcircle} >
+							<PercentageCircle borderWidth={5} radius={25} percent={33} color={Colors.navy}></PercentageCircle>
+							<ProximaText> Fiber </ProximaText>
+						</View>
+						<View style={styles.pcircle} >
+							<PercentageCircle borderWidth={5} radius={25} percent={61} color={Colors.navy}></PercentageCircle>
+							<ProximaText> Iron </ProximaText>
+						</View>
+					</View>
+				</View>
+				<View style={styles.mealPlanMeals}>
+					<ProximaText style = {styles.header}> Breakfast </ProximaText>
+					<View style={styles.meals}>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/eggmuffin.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Healthy Breakfast Egg Muffins</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/eggmuffin.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Healthy Breakfast Egg Muffins</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+					</View>
+					<ProximaText style = {styles.header}> Lunch/Dinner </ProximaText>
+					<View style={styles.meals}>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/chickentacoavocado.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Chicken Taco Avocado</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/chickentacoavocado.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Chicken Taco Avocado</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+					</View>
+					<ProximaText style = {styles.header}> Snacks </ProximaText>
+					<View style={styles.meals}>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/granola.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Granola</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => navigate('Dice')} style={styles.item}>
+			        <Image style={styles.image} source={require('../../assets/images/recipes/granola.png')} />
+							<View style={styles.itemText}>
+								<ProximaText>Granola</ProximaText>
+								<ProximaText>Description of recipes </ProximaText>
+							</View>
+						</TouchableOpacity>
+					</View>
+				</View>
 			</ScrollView>
 		);
 	}
@@ -26,4 +107,65 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+	datebar: {
+		borderTopWidth:2,
+		borderBottomWidth:2,
+	},
+	date: {
+		textAlign: 'center',
+		fontSize:17,
+		marginTop:5,
+		marginBottom:5,
+	},
+	header: {
+		fontSize:17,
+		marginTop:10,
+		marginBottom:10,
+		textAlign: 'left',
+	},
+	mealplanBar: {
+		alignItems:'center',
+		backgroundColor: Colors.lemon,
+		flex:1,
+		flexDirection:'column',
+		marginTop:10,
+		marginBottom:5,
+		paddingTop:5,
+		paddingBottom: 5,
+		paddingLeft:5,
+		paddingRight: 5,
+	},
+	pcircles: {
+		flexDirection: 'row',
+		marginTop:5,
+		marginBottom:5,
+	},
+	pcircle: {
+		flexDirection:'column',
+		marginLeft:10,
+		marginRight:10,
+	},
+	mealPlanMeals: {
+
+	},
+	meals: {
+		flexDirection:'column',
+	},
+	item: {
+		marginTop: 10,
+		marginRight:5,
+		marginLeft:5,
+		marginBottom:5,
+		paddingBottom:5,
+		flex:1,
+		flexDirection: 'row',
+	},
+	image: {
+		height: 165,
+		width: 165,
+		marginBottom:5,
+	},
+	itemText: {
+
+	},
 });
